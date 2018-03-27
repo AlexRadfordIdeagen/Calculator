859f00ef3490a598eb4a1b82cb9d5f151a40a46f
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-03-27 13:23:24 +0100

    Introduced the multiplacation function and inputs.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -11,6 +11,19 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("Please enter a number");
+
+            string firstString = "";
+            firstString = Console.ReadLine();
+            int firstValue = int.Parse(firstString);
+
+            Console.WriteLine("Please enter a second number");
+
+            string secondString = "";
+            secondString = Console.ReadLine();
+            int secondValue = int.Parse(secondString);
+
+            Console.WriteLine(firstValue * secondValue);
 
             Console.ReadLine();
         }
